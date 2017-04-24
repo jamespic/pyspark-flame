dda5578dc39c89479149f583e6f2a142719aabf3
James Pickering <james.pickering@xml-solutions.com>
2017-04-24 16:09:49 +0100

    Minor changes to prepare for PyPI

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author_email='james_pic@hotmail.com',
     license='MIT',
     url='https://github.com/jamespic/pyspark-flame',
+    download_url='https://github.com/jamespic/pyspark-flame/archive/0.1.0.tar.gz',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     test_suite='test'

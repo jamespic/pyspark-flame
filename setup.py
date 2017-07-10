6711665fda8ca4ecdffb5aa10076ec8dc1dd7775
James Pickering <james.pickering@xml-solutions.com>
2017-07-10 15:00:22 +0100

    Fix README.md upload

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
-
 from setuptools import setup, find_packages
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 setup(
     name='pyspark-flame',
-    version='0.2.0',
+    version='0.2.2',
     description='A low-overhead sampling profiler for PySpark, that outputs Flame Graphs',
+    long_description=long_description,
     author='James Pickering',
     author_email='james_pic@hotmail.com',
     license='MIT',
     url='https://github.com/jamespic/pyspark-flame',
-    download_url='https://github.com/jamespic/pyspark-flame/archive/0.1.2.tar.gz',
+    download_url='https://github.com/jamespic/pyspark-flame/archive/0.2.2.tar.gz',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     scripts=['FlameGraph/flamegraph.pl'],
+    setup_requires=['pypandoc'],
     test_suite='test'
 )

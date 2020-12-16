88a5195ec1f0a686828dd87696d0afa689b3eab9
James Pickering <james.pickering@airelogic.com>
2020-12-16 15:05:05 +0000

    Update packaging and testing

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
+long_description = open('README.md').read()
 
 setup(
     name='pyspark-flame',
-    version='0.2.6',
     description='A low-overhead sampling profiler for PySpark, that outputs Flame Graphs',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='James Pickering',
     author_email='james_pic@hotmail.com',
     license='MIT',
     url='https://github.com/jamespic/pyspark-flame',
-    download_url='https://github.com/jamespic/pyspark-flame/archive/0.2.4.tar.gz',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     scripts=['FlameGraph/flamegraph.pl'],
     install_requires=['pyspark'],
-    setup_requires=['pypandoc'],
-    test_suite='test'
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    test_suite='test',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
 )
